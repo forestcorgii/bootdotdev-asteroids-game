1e8520229aa09717ed541e1c9c297d7e77a83fb9
forestcorgii <seanivanf@gmail.com>
2024-09-01 12:27:47 +0800

    Automate drawing and updating objects using pygame.sprite.Group

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import pygame
+from asteroid import Asteroid
+from asteroidField import AsteroidField
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	asteroidField = AsteroidField()
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
+		for thing in updatable:
+			thing.update(dt)
+		
 		screen.fill((0,0,0))
-		pygame.display.flip()
 
+		for thing in drawable:
+			thing.draw(screen)
+		
+		pygame.display.flip()
+		
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
